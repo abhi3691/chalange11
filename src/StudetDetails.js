04fd1f10f71b4afec8b6055ffed1667c1f924483
ABHINAND <abhinandazokdio@gmail.com>
2021-10-26 12:04:36 +0530

    updated

--- src/StudetDetails.js
+++ src/StudetDetails.js
@@ -1,14 +1,17 @@
 import React,{ Component,useState } from 'react';
 import { View, Text, StyleSheet,TextInput,Image,TouchableHighlight,ImageBackground ,} from 'react-native';
 
+import { Picker } from '@react-native-picker/picker';
 
 
 
 export default class StudentDetails extends Component  {
  
+ 
   render(){ 
+   
+  
     
-
   return (
       
     <View style={styles.container}>
@@ -39,8 +42,51 @@ export default class StudentDetails extends Component  {
                
                 <TextInput style={styles.inputTextStyle} keyboardType='name-phone-pad' placeholder='Full name' placeholderTextColor='#446270'  ></TextInput>
                 <TextInput style={styles.inputTextStyle} keyboardType= 'email-address' placeholder='Email' placeholderTextColor='#446270' ></TextInput>
-                <TextInput style={styles.inputTextStyle} keyboardType='number-pad' ></TextInput>
-                <TextInput style={styles.inputTextStyle} keyboardType='number-pad' ></TextInput>
+               <Picker style={styles.inputTextStyle} 
+            
+               >
+                 
+                 <Picker.Item   label="Assam" value = "Assam" />
+                 <Picker.Item label="Goa" value = "Goa" />
+                 <Picker.Item label="Madhya Pradesh" value = "Madhya Pradesh" />
+                 <Picker.Item label="Meghalaya" value = "Meghalaya" />
+                 <Picker.Item label="Mizoram" value = "Mizoram" />
+                 <Picker.Item label="Delhi" value = "Delhi" />
+                 <Picker.Item label="Sikkim" value = "Sikkim" />
+                 <Picker.Item label="Andhra Pradesh" value = "Andhra Pradesh" />
+                 <Picker.Item label="Arunachal Pradesh" value = "Arunachal Pradesh" />
+                 <Picker.Item label="Bihar" value = "Bihar" />
+                 <Picker.Item label="Chhattisgarh" value = "Chhattisgarh" />
+                 <Picker.Item label="Gujarat" value = "Gujarat" />
+                 <Picker.Item label="Haryana" value = "Haryana" />
+                <Picker.Item label="Himachal Pradesh" value = "Himachal Pradesh" />
+                <Picker.Item label="Jammu and Kashmir" value = "Jammu and Kashmir" />
+                <Picker.Item label="Himachal Pradesh" value = "Himachal Pradesh" />
+                <Picker.Item label="Jharkhand" value = "Jharkhand" />
+                <Picker.Item label="Karnataka" value = "Karnataka" />
+                <Picker.Item label="Kerala" value = "Kerala" />
+                <Picker.Item label="Maharashtra" value = "Maharashtra" />
+                <Picker.Item label="Nagaland" value = "Nagaland" />
+                <Picker.Item label="Odisha" value = "Odisha" />
+                <Picker.Item label="Punjab" value = "Punjab" />
+                <Picker.Item label="Odisha" value = "Odisha" />
+                <Picker.Item label="Rajasthan" value = "Rajasthan" />
+                <Picker.Item label="Tamil Nad" value = "Tamil Nad" />
+                <Picker.Item label="Uttarakhand" value = "Uttarakhand" />
+                <Picker.Item label="Telangana" value = "Telangana" />
+                <Picker.Item label="Tripura" value = "Tripura" />
+                <Picker.Item label="Telangana" value = "Telangana" />
+                <Picker.Item label="Andaman and Nicobar Islands" value = "Andaman and Nicobar Islands" />
+                <Picker.Item label="Chandigarh" value = "Chandigarh" />
+                <Picker.Item label="Dadra and Nagar Haveli" value = "Dadra and Nagar Haveli" />
+                <Picker.Item label="Daman and Di" value = "Daman and Di" />
+                <Picker.Item label="Lakshadweep" value = "Lakshadweep" />
+                <Picker.Item label="Puducherry" value = "Puducherry" />
+                <Picker.Item label="Uttar Pradesh" value = "Uttar Pradesh" />
+                <Picker.Item label="West Bengal" value = "West Bengal" />
+
+               </Picker>
+               <TextInput style={styles.inputTextStyle} keyboardType='number-pad' placeholder='Pin Code' placeholderTextColor='#446270'  ></TextInput>
            
 
               
